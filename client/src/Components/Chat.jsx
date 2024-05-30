19f37e81962e398b5dcf4dd77528481992d2e15e
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-05-30 18:45:56 +0300

    add date in the chat ballon and add  a new design with tailwind

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -11,6 +11,11 @@ const Chat = ({ username = "Guest", room, socket, randomChatColor }) => {
   }, [socket]);
 
   const sendMessage = async () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const formattedTime = `${hours}:${minutes}`;
+
     if (message.trim() === "") {
       return;
     }
@@ -18,8 +23,7 @@ const Chat = ({ username = "Guest", room, socket, randomChatColor }) => {
       username: username,
       message: message,
       room: room,
-      date:
-        new Date(Date.now).getHours() + ":" + new Date(Date.now).getMinutes(),
+      date: formattedTime,
       randomChatColor,
     };
     await socket.emit("message", messageContent);
@@ -40,54 +44,76 @@ const Chat = ({ username = "Guest", room, socket, randomChatColor }) => {
   };
 
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="w-1/5 h-[600px] bg-zinc-400 flex flex-col justify-between">
-        <div className="bg-green-500 h-10 p-2">
-          <p className="text-white font-bold">Welcome To Chat Room</p>
-        </div>
-        <div className="w-full h-[400px] overflow-y-auto">
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 p-10">
+      <div className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-1/3 bg-white shadow-xl flex flex-col justify-between rounded-l">
+        <div className="w-full h-[600px] overflow-y-auto mt-3">
           {messageList &&
             messageList.map((msg, i) => (
               <div
                 className={`${
-                  username === msg.username ? "flex justify-end" : ":"
+                  username === msg.username ? "flex justify-end" : ""
                 }`}
               >
                 <div
                   className={`${
                     username === msg.username
-                      ? "bg-green-600"
-                      : msg.randomChatColor
-                  } w-2/3 h-12 p-2 text-white text-sm m-2 rounded-xl rounded-br-none`}
+                      ? "bg-customBlue rounded-br-none text-white"
+                      : "bg-customMessage rounded-bl-none text-black"
+                  } w-max h-full p-3  text-sm m-2 rounded-xl text-pretty break-all `}
                 >
-                  <div>{msg.message}</div>
-                  <div className="w-full flex justify-end text-xs">
-                    {msg.username === ""
-                      ? `Guest-${generateRandomName()}`
-                      : msg.username}
+                  {username === msg.username ? (
+                    <p></p>
+                  ) : (
+                    <p className={`${msg.randomChatColor} font-bold`}>
+                      {msg.username === ""
+                        ? `Guest-${generateRandomName()}`
+                        : msg.username}
+                    </p>
+                  )}
+                  <div className="w-[190px]">{msg.message}</div>
+                  <div>
+                    <div
+                      className={`${
+                        username === msg.username
+                          ? "justify-end"
+                          : "justify-end"
+                      } flex text-xs justify-end`}
+                    >
+                      <p>{msg.date}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             ))}
         </div>
-        <div className="flex flex-row items-center justify-start mb-4">
+        <div className="flex flex-row items-center justify-start bg-zinc-300">
           <input
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className="w-72 h-10 mt-5 rounded-xl p-2 ml-4"
-            placeholder="Message"
+            className="w-full h-10 mt-5 rounded-l p-2 ml-4 mb-4 text-xs"
+            placeholder="Type you message..."
             type="text"
           />
-          <button
+
+          <div
             onClick={sendMessage}
-            className="ml-5 font-bold h-5 text-2xl text-green-500 cursor-pointer rounded-xl"
+            className="ml-3 mr-3 mt-5 font-bold text-green-500 cursor-pointer rounded-xl bg-gray-800 p-2 mb-4"
           >
-            <img
-              className="border p-2 rounded-xl bg-white"
-              src="./arrow-25-24.png"
-              alt=""
-            />
-          </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
