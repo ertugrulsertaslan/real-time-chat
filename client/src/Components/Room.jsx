39d76478ef4dc6780f28a4094dc541d3fb5ee77f
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-05-30 18:48:02 +0300

    add new design with tailwind

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -12,36 +12,34 @@ const Room = ({
     socket.emit("room", room);
     setChatScreen(true);
   };
-
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="w-1/3 h-[500px] bg-zinc-500">
-        <div className="text-center mt-5 font-bold text-emerald-500">
-          Welcome To Chat
-        </div>
-        <div className="flex flex-col justify-center items-center h-full">
-          <h2 className="font-bold text-3xl">Username :</h2>
-          <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-1/3 h-10 mt-5 rounded-xl p-1"
-            placeholder="Username"
-            type="text"
-          ></input>
-          <h2 className="mt-10 font-bold text-3xl">Room:</h2>
-          <input
-            value={room}
-            onChange={(e) => setRoom(e.target.value)}
-            className=" w-1/3 h-10 mt-5 rounded-xl p-1"
-            placeholder="Room"
-            type="text"
-          ></input>
-          <button
-            onClick={handleClick}
-            className="mt-10 font-bold h-10 text-3xl text-green-500 cursor-pointer rounded-xl name"
-          >
-            Join Room
-          </button>
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+      <div className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-1/3 bg-white shadow-xl flex flex-col justify-between rounded-l">
+        <div className="w-full h-[600px] overflow-y-auto mt-3">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h2 className="font-custom text-3xl text-customBlue">Username</h2>
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-1/2 h-10 mt-5 rounded-xl p-5 border-blue-300 focus:border-blue-500 border-2 focus:outline-none"
+              placeholder="Username"
+              type="text"
+            ></input>
+            <h2 className="mt-10 font-custom text-3xl text-customBlue">Room</h2>
+            <input
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+              className=" w-1/2 h-10 mt-5 rounded-xl p-5 border-blue-300 focus:border-blue-500 border-2 focus:outline-none"
+              placeholder="Room"
+              type="text"
+            ></input>
+            <button
+              onClick={handleClick}
+              className="mt-20 h-10 text-3xl text-green-500 cursor-pointer rounded-xl name font-custom"
+            >
+              Join Room
+            </button>
+          </div>
         </div>
       </div>
     </div>
