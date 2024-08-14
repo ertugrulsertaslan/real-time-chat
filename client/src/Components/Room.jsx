1342e5f52ba27b446012176ee5dffb9efdc8b994
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-08-14 13:09:49 +0300

    add new UI to room page with mui

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from "react";
-
+import { React } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container, Box, Paper } from "@mui/material";
 const Room = ({ username, room, setUsername, setRoom, error, socket }) => {
   const generateRandomName = () => {
     const length = 6;
@@ -22,37 +25,77 @@ const Room = ({ username, room, setUsername, setRoom, error, socket }) => {
     socket.emit("room", { room, username: _username });
   };
   return (
-    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
-      <div className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-1/3 bg-white shadow-xl flex flex-col justify-between rounded-l">
-        <div className="w-full h-[600px] overflow-y-auto mt-3">
-          <div className="flex flex-col justify-center items-center h-full">
-            <h2 className="font-custom text-3xl text-customBlue">Username</h2>
-            {error && <p className="text-red-500 font-bold">{error}</p>}
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-1/2 h-10 mt-5 rounded-xl p-5 border-blue-300 focus:border-blue-500 border-2 focus:outline-none"
-              placeholder="Username"
-              type="text"
-            ></input>
-            <h2 className="mt-10 font-custom text-3xl text-customBlue">Room</h2>
-            <input
-              value={room}
-              onChange={(e) => setRoom(e.target.value)}
-              className=" w-1/2 h-10 mt-5 rounded-xl p-5 border-blue-300 focus:border-blue-500 border-2 focus:outline-none"
-              placeholder="Room"
-              type="text"
-            ></input>
-            <button
-              onClick={handleClick}
-              className="mt-20 h-10 text-3xl text-green-500 cursor-pointer rounded-xl name font-custom"
-            >
-              Join Room
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "white",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          borderRadius: 2,
+          backgroundColor: "white",
+          width: "100%",
+          maxWidth: 400,
+        }}
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          width="100%"
+          sx={{ gap: 8 }}
+        >
+          <Typography
+            variant="h4"
+            component="h1"
+            align="center"
+            gutterBottom
+            sx={{ marginTop: 10, color: "primary.main" }}
+          >
+            Real Chat
+          </Typography>
+          <TextField
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            fullWidth
+          />
+          <TextField
+            label="Room Number"
+            variant="outlined"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            fullWidth
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleClick}
+            sx={{
+              marginTop: 2,
+              marginBottom: 8,
+              width: {
+                xs: "100%", // Extra küçük ekranlarda %100 genişlik
+                sm: "80%", // Küçük ekranlarda %80 genişlik
+                md: "70%", // Orta ekranlarda %60 genişlik
+                lg: "70%", // Büyük ekranlarda %40 genişlik
+                xl: "70%", // Extra büyük ekranlarda %30 genişlik
+              },
+            }}
+          >
+            Join Room
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 export default Room;
